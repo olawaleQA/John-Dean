e76e4c05898b622011913e8b1c09c53f63c7af4e
olawaleQA <olawaleolowo19@gmail.com>
2024-11-10 22:04:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var school = "Lagos State University"
+var name = "Johnny Cane"
+console.log(name)
+
+console.log(school)
+
+let place = "ibadan"
+console.log(place)
+
+const age = "25"
+console.log(age)
